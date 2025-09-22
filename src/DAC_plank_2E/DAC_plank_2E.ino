c6f13248429892de0429334fb1e6c2a9a3362732
h4lfw1t <graysonofgotham2112@gmail.com>
2025-09-22 15:58:42 -0400

    Add initial data files and notebook for LED frequency experiment

--- src/DAC_plank_2E/DAC_plank_2E.ino
+++ src/DAC_plank_2E/DAC_plank_2E.ino
@@ -15,58 +15,57 @@ template <typename T>
 void printSerialCSV(T value, bool eol = false);
 
 void setup() {
-  Serial.begin(9600);
-  dac.begin(0x62);
-  for (int i = 0; i < 35; i++) {
-    Serial.print("-");
-  }
-  Serial.println();
-  Serial.println("DAC mounted to 0x62");
+    Serial.begin(9600);
+    dac.begin(0x62);
+    for (int i = 0; i < 35; i++) {
+        Serial.print("-");
+    }
+    Serial.println();
+    Serial.println("DAC mounted to 0x62");
 }
 
 void loop() {
-  if (not hasRun) {
-    Serial.println("Outputting voltage series.");
-    Serial.println("out,A0,A1,A2,A0-A1,A1-A2,current");
-    for (int i = 0; i < 51; i++) {
-      // Ramp voltage from 0V to 5V with a 0.1 step
-      int outputSignal = float_map(i, 0, 51, 0, 4095);
-      dac.setVoltage(outputSignal, false);
-      outputVoltage = i / 10.0;
-      Serial.print(String(outputVoltage) + ",");
+    if (not hasRun) {
+        Serial.println("Outputting voltage series.");
+        Serial.println("out,A0,A1,A2,A0-A1,A1-A2,current");
+        for (int i = 0; i < 51; i++) {
+        // Ramp voltage from 0V to 5V with a 0.1 step
+        int outputSignal = float_map(i, 0, 51, 0, 4095);
+        dac.setVoltage(outputSignal, false);
+        outputVoltage = i / 10.0;
+        Serial.print(String(outputVoltage) + ",");
 
-      delay(50);
+        delay(50);
 
-      float signalA0 = readA0();
-      float signalA1 = readA1();
-      float signalA2 = readA2();
+        float signalA0 = readA0();
+        float signalA1 = readA1();
+        float signalA2 = readA2();
 
-//      measuredInputVoltage = float_map(measuredInputSignal, 0, 1023, 0, 50) / 10.0;
-      printSerialCSV(signalA0);
-      printSerialCSV(signalA1);
-      printSerialCSV(signalA2);
-      printSerialCSV(signalA0 - signalA1);
-      printSerialCSV(signalA1 - signalA2);
-      printSerialCSV(((signalA0 - signalA1) / RESISTOR), true);
+        printSerialCSV(signalA0);
+        printSerialCSV(signalA1);
+        printSerialCSV(signalA2);
+        printSerialCSV(signalA0 - signalA1);
+        printSerialCSV(signalA1 - signalA2);
+        printSerialCSV(((signalA0 - signalA1) / RESISTOR), true);
 
-      delay(50);
-    }
+        delay(50);
+        }
     Serial.println("Finished");
     hasRun = true;
-  }
+    }
 }
 
 // - Helpers to read specific pins - 
 float readA0() {
-  return readAnalogToVoltage(A0);
+    return readAnalogToVoltage(A0);
 }
 
 float readA1() {
-  return readAnalogToVoltage(A1);
+    return readAnalogToVoltage(A1);
 }
 
 float readA2() {
-  return readAnalogToVoltage(A2);
+    return readAnalogToVoltage(A2);
 }
 
 /*
@@ -74,19 +73,19 @@ float readA2() {
  * representation between 0-5V
  */
 float readAnalogToVoltage(unsigned long pin) {
-  return float_map(analogRead(pin), 0, 1023, 0, 50) / 10.0;
+    return float_map(analogRead(pin), 0, 1023, 0, 50) / 10.0;
 }
 
 // Custom map function to work with floats instead of integers
 float float_map(float value, float fromLow, float fromHigh, float toLow, float toHigh) {
-  return toLow + ((value - fromLow) * (toHigh - toLow)) / (fromHigh - fromLow);
+    return toLow + ((value - fromLow) * (toHigh - toLow)) / (fromHigh - fromLow);
 }
 
 template <typename T>
 void printSerialCSV(T value, bool eol = false) {
-  if (eol) {
-    Serial.println(value);
-  } else {
-    Serial.print(String(value) + ",");
-  }
+    if (eol) {
+        Serial.println(value);
+    } else {
+        Serial.print(String(value) + ",");
+    }
 }
